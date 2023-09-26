241ae34288cc67012987b8e2e3fa196255907e98
Бернякович Никита Игоревич <23622@dune.edu>
2023-09-26 11:18:41 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+   double a, b , c, m;
+   cout<<" m= ";
+   cin>>m;
+   cout<<"\n a= ";
+   cin>> a;
+   cout<<" \n b= ";
+   cin>>b;
+   c= 1- (a+b);
+   a = a*m;
+   b = b*m ;
+   c = m*c;
+   cout<< "\n a: = "<<a<<"\n b: = "<<b<<"\n c:= "<<c;
+
+
+
+}
